fc3b8fc6fe75cce41080686dfd2a313f0cf13447
yamori-masato <yamorimasato@gmail.com>
2021-02-06 00:37:19 +0900

    useState

---
+++ src/Counter.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const Counter: React.FC<{}> = () => {
+  const initialValue: any = 0;
+  const [value, setValue] = useState<number>(initialValue);
+
+  const increment = () => setValue(value + 1);
+  const decrement = () => setValue(value - 1);
+
+  return (
+    <div>
+      <div>value: {value}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+    </div>
+  );
+};
+
+export default Counter;
